96714130e2640658b1f6e462cde8257322336e70
Marcelo Gonçalves <122677916+Ch1c4n0@users.noreply.github.com>
2024-10-29 14:23:51 -0300

    Update script2.ps1

--- script2.ps1
+++ script2.ps1
@@ -27,30 +27,13 @@ function Get-WindowsAutopilotInfoCSV {
     return $outputFile
 }
 
-# Função para enviar arquivo por e-mail
-function Send-EmailWithAttachment($filePath, $smtpServer, $smtpPort, $smtpUser, $smtpPassword, $to, $subject, $body) {
-    $securePassword = ConvertTo-SecureString $smtpPassword -AsPlainText -Force
-    $credentials = New-Object System.Management.Automation.PSCredential ($smtpUser, $securePassword)
-    $message = New-Object System.Net.Mail.MailMessage
-    $message.From = $smtpUser
-    $message.To.Add($to)
-    $message.Subject = $subject
-    $message.Body = $body
-    $attachment = New-Object System.Net.Mail.Attachment($filePath)
-    $message.Attachments.Add($attachment)
-    $smtp = New-Object System.Net.Mail.SmtpClient($smtpServer, $smtpPort)
-    $smtp.EnableSsl = $true
-    $smtp.Credentials = $credentials
-    $smtp.Send($message)
-}
-
 # Criar a interface gráfica
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 $form = New-Object System.Windows.Forms.Form
-$form.Text = "Autopilot"
-$form.Size = New-Object System.Drawing.Size(400, 500)
+$form.Text = "Autopilot - Marcelo Goncalves v1.0 - Intune Lovers!"
+$form.Size = New-Object System.Drawing.Size(400, 400)
 
 $buttonInstallGraph = New-Object System.Windows.Forms.Button
 $buttonInstallGraph.Text = "Install Microsoft Graph"
@@ -59,53 +42,49 @@ $buttonInstallGraph.Size = New-Object System.Drawing.Size(200, 30)
 $buttonInstallGraph.BackColor = [System.Drawing.Color]::Yellow
 $form.Controls.Add($buttonInstallGraph)
 
-$buttonInstallPnP = New-Object System.Windows.Forms.Button
-$buttonInstallPnP.Text = "PNP Sharepoint"
-$buttonInstallPnP.Location = New-Object System.Drawing.Point(100, 70)
-$buttonInstallPnP.Size = New-Object System.Drawing.Size(200, 30)
-$buttonInstallPnP.BackColor = [System.Drawing.Color]::Purple
-$form.Controls.Add($buttonInstallPnP)
-
 $buttonLogin = New-Object System.Windows.Forms.Button
 $buttonLogin.Text = "Login"
-$buttonLogin.Location = New-Object System.Drawing.Point(150, 120)
+$buttonLogin.Location = New-Object System.Drawing.Point(150, 70)
 $buttonLogin.Size = New-Object System.Drawing.Size(100, 30)
 $form.Controls.Add($buttonLogin)
 
 $buttonAutopilotGroupTag = New-Object System.Windows.Forms.Button
 $buttonAutopilotGroupTag.Text = "Autopilot Online With Group Tag"
-$buttonAutopilotGroupTag.Location = New-Object System.Drawing.Point(100, 170)
+$buttonAutopilotGroupTag.Location = New-Object System.Drawing.Point(100, 120)
 $buttonAutopilotGroupTag.Size = New-Object System.Drawing.Size(200, 30)
 $buttonAutopilotGroupTag.Enabled = $false
 $form.Controls.Add($buttonAutopilotGroupTag)
 
 $buttonAutopilotCSV = New-Object System.Windows.Forms.Button
 $buttonAutopilotCSV.Text = "Windows Autopilot CSV"
-$buttonAutopilotCSV.Location = New-Object System.Drawing.Point(100, 220)
+$buttonAutopilotCSV.Location = New-Object System.Drawing.Point(100, 170)
 $buttonAutopilotCSV.Size = New-Object System.Drawing.Size(200, 30)
 $buttonAutopilotCSV.Enabled = $false
 $form.Controls.Add($buttonAutopilotCSV)
 
-$buttonAutopilotEmail = New-Object System.Windows.Forms.Button
-$buttonAutopilotEmail.Text = "Windows Autopilot E-mail"
-$buttonAutopilotEmail.Location = New-Object System.Drawing.Point(100, 270)
-$buttonAutopilotEmail.Size = New-Object System.Drawing.Size(200, 30)
-$buttonAutopilotEmail.Enabled = $false
-$form.Controls.Add($buttonAutopilotEmail)
-
 $labelStatus = New-Object System.Windows.Forms.Label
-$labelStatus.Location = New-Object System.Drawing.Point(150, 150)
+$labelStatus.Location = New-Object System.Drawing.Point(150, 50)
 $labelStatus.Size = New-Object System.Drawing.Size(100, 20)
 $form.Controls.Add($labelStatus)
 
 # Evento de clique do botão Install Microsoft Graph
 $buttonInstallGraph.Add_Click({
-    Start-Process powershell -ArgumentList "Install-Module Microsoft.Graph -AllowClobber -Force" -NoNewWindow
-})
+    $installForm = New-Object System.Windows.Forms.Form
+    $installForm.Text = "Installing Microsoft Graph"
+    $installForm.Size = New-Object System.Drawing.Size(300, 100)
 
-# Evento de clique do botão PNP Sharepoint
-$buttonInstallPnP.Add_Click({
-    Start-Process powershell -ArgumentList "Install-Module -Name PnP.PowerShell -Force" -NoNewWindow
+    $labelInstall = New-Object System.Windows.Forms.Label
+    $labelInstall.Text = "Installing Microsoft Graph, please wait..."
+    $labelInstall.AutoSize = $true
+    $labelInstall.Location = New-Object System.Drawing.Point(10, 10)
+    $installForm.Controls.Add($labelInstall)
+
+    $installForm.Show()
+
+    Start-Process powershell -ArgumentList "Install-Module Microsoft.Graph -AllowClobber -Force" -Wait
+
+    $installForm.Close()
+    [System.Windows.Forms.MessageBox]::Show("Microsoft Graph installed successfully!", "Installation Complete", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
 })
 
 # Evento de clique do botão de login
@@ -114,12 +93,31 @@ $buttonLogin.Add_Click({
         Connect-MgGraphWithScopes
         $buttonAutopilotGroupTag.Enabled = $true
         $buttonAutopilotCSV.Enabled = $true
-        $buttonAutopilotEmail.Enabled = $true
-        $labelStatus.Text = "SUCCESS"
-        $labelStatus.ForeColor = [System.Drawing.Color]::Green
+        $successForm = New-Object System.Windows.Forms.Form
+        $successForm.Text = "Login Status"
+        $successForm.Size = New-Object System.Drawing.Size(300, 100)
+        $successForm.BackColor = [System.Drawing.Color]::Green
+
+        $labelSuccess = New-Object System.Windows.Forms.Label
+        $labelSuccess.Text = "SUCCESS"
+        $labelSuccess.AutoSize = $true
+        $labelSuccess.Location = New-Object System.Drawing.Point(10, 10)
+        $successForm.Controls.Add($labelSuccess)
+
+        $successForm.ShowDialog()
     } catch {
-        $labelStatus.Text = "FAIL"
-        $labelStatus.ForeColor = [System.Drawing.Color]::Red
+        $failForm = New-Object System.Windows.Forms.Form
+        $failForm.Text = "Login Status"
+        $failForm.Size = New-Object System.Drawing.Size(300, 100)
+        $failForm.BackColor = [System.Drawing.Color]::Red
+
+        $labelFail = New-Object System.Windows.Forms.Label
+        $labelFail.Text = "FAIL"
+        $labelFail.AutoSize = $true
+        $labelFail.Location = New-Object System.Drawing.Point(10, 10)
+        $failForm.Controls.Add($labelFail)
+
+        $failForm.ShowDialog()
     }
 })
 
@@ -160,115 +158,6 @@ $buttonAutopilotGroupTag.Add_Click({
 $buttonAutopilotCSV.Add_Click({
     $outputFile = Get-WindowsAutopilotInfoCSV
     [System.Windows.Forms.MessageBox]::Show("CSV file created at $outputFile")
-})
-
-# Evento de clique do botão Windows Autopilot E-mail
-$buttonAutopilotEmail.Add_Click({
-    $outputFile = Get-WindowsAutopilotInfoCSV
-
-    $inputForm = New-Object System.Windows.Forms.Form
-    $inputForm.Text = "Enter E-mail Details"
-    $inputForm.Size = New-Object System.Drawing.Size(300, 350)
-
-    $labelSmtp = New-Object System.Windows.Forms.Label
-    $labelSmtp.Text = "SMTP Server:"
-    $labelSmtp.Location = New-Object System.Drawing.Point(10, 20)
-    $inputForm.Controls.Add($labelSmtp)
-
-    $textBoxSmtp = New-Object System.Windows.Forms.TextBox
-    $textBoxSmtp.Location = New-Object System.Drawing.Point(100, 20)
-    $textBoxSmtp.Size = New-Object System.Drawing.Size(180, 20)
-    $textBoxSmtp.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
-    $inputForm.Controls.Add($textBoxSmtp)
-
-    $labelPort = New-Object System.Windows.Forms.Label
-    $labelPort.Text = "SMTP Port:"
-    $labelPort.Location = New-Object System.Drawing.Point(10, 60)
-    $inputForm.Controls.Add($labelPort)
-
-    $textBoxPort = New-Object System.Windows.Forms.TextBox
-    $textBoxPort.Location = New-Object System.Drawing.Point(100, 60)
-    $textBoxPort.Size = New-Object System.Drawing.Size(180, 20)
-    $textBoxPort.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
-    $inputForm.Controls.Add($textBoxPort)
-
-    $labelUsername = New-Object System.Windows.Forms.Label
-    $labelUsername.Text = "Username:"
-    $labelUsername.Location = New-Object System.Drawing.Point(10, 100)
-    $inputForm.Controls.Add($labelUsername)
-
-    $textBoxUsername = New-Object System.Windows.Forms.TextBox
-    $textBoxUsername.Location = New-Object System.Drawing.Point(100, 100)
-    $textBoxUsername.Size = New-Object System.Drawing.Size(180, 20)
-    $textBoxUsername.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
-    $inputForm.Controls.Add($textBoxUsername)
-
-    $labelPassword = New-Object System.Windows.Forms.Label
-    $labelPassword.Text = "Password:"
-    $labelPassword.Location = New-Object System.Drawing.Point(10, 140)
-    $inputForm.Controls.Add($labelPassword)
-
-    $textBoxPassword = New-Object System.Windows.Forms.TextBox
-    $textBoxPassword.Location = New-Object System.Drawing.Point(100, 140)
-    $textBoxPassword.Size = New-Object System.Drawing.Size(180, 20)
-    $textBoxPassword.PasswordChar = '*'
-    $textBoxPassword.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
-    $inputForm.Controls.Add($textBoxPassword)
-
-    $labelTo = New-Object System.Windows.Forms.Label
-    $labelTo.Text = "To:"
-    $labelTo.Location = New-Object System.Drawing.Point(10, 180)
-    $inputForm.Controls.Add($labelTo)
-
-    $textBoxTo = New-Object System.Windows.Forms.TextBox
-    $textBoxTo.Location = New-Object System.Drawing.Point(100, 180)
-    $textBoxTo.Size = New-Object System.Drawing.Size(180, 20)
-    $textBoxTo.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
-    $inputForm.Controls.Add($textBoxTo)
-
-    $labelSubject = New-Object System.Windows.Forms.Label
-    $labelSubject.Text = "Subject:"
-    $labelSubject.Location = New-Object System.Drawing.Point(10, 220)
-    $inputForm.Controls.Add($labelSubject)
-
-    $textBoxSubject = New-Object System.Windows.Forms.TextBox
-    $textBoxSubject.Location = New-Object System.Drawing.Point(100, 220)
-    $textBoxSubject.Size = New-Object System.Drawing.Size(180, 20)
-    $textBoxSubject.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
-    $inputForm.Controls.Add($textBoxSubject)
-
-    $labelBody = New-Object System.Windows.Forms.Label
-    $labelBody.Text = "Body:"
-    $labelBody.Location = New-Object System.Windows.Forms.Label
-    $labelBody.Location = New-Object System.Drawing.Point(10, 260)
-    $inputForm.Controls.Add($labelBody)
-
-    $textBoxBody = New-Object System.Windows.Forms.TextBox
-    $textBoxBody.Location = New-Object System.Drawing.Point(100, 260)
-    $textBoxBody.Size = New-Object System.Drawing.Size(180, 20)
-    $textBoxBody.Anchor = [System.Windows.Forms.AnchorStyles]::Top -bor [System.Windows.Forms.AnchorStyles]::Left -bor [System.Windows.Forms.AnchorStyles]::Right
-    $inputForm.Controls.Add($textBoxBody)
-
-    $buttonOK = New-Object System.Windows.Forms.Button
-    $buttonOK.Text = "OK"
-    $buttonOK.Location = New-Object System.Drawing.Point(100, 300)
-    $buttonOK.Size = New-Object System.Drawing.Size(75, 30)
-    $inputForm.Controls.Add($buttonOK)
-
-    $buttonOK.Add_Click({
-        $smtpServer = $textBoxSmtp.Text
-        $smtpPort = $textBoxPort.Text
-        $username = $textBoxUsername.Text
-        $password = $textBoxPassword.Text
-        $to = $textBoxTo.Text
-        $subject = $textBoxSubject.Text
-        $body = $textBoxBody.Text
-        $inputForm.Close()
-        Send-EmailWithAttachment $outputFile $smtpServer $smtpPort $username $password $to $subject $body
-        [System.Windows.Forms.MessageBox]::Show("CSV file sent to $to")
-    })
-
-    $inputForm.ShowDialog()
 })
 
 # Exibir o formulário

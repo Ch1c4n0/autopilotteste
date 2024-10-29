16650ead7d250687eb124bdd79bfcb8bc055e61d
Marcelo Gonçalves <122677916+Ch1c4n0@users.noreply.github.com>
2024-10-29 15:47:24 -0300

    Update script2.ps1

--- script2.ps1
+++ script2.ps1
@@ -1,3 +1,14 @@
+# Função para conectar ao Microsoft Graph
+function Connect-MgGraphWithScopes {
+    Connect-MgGraph -Scopes "User.Read.All", "Group.ReadWrite.All", "Directory.ReadWrite.All", "DeviceManagementManagedDevices.ReadWrite.All", "DeviceManagementConfiguration.ReadWrite.All", "DeviceManagementServiceConfig.ReadWrite.All", "DeviceManagementApps.ReadWrite.All"
+}
+
+# Função para obter perfis de implantação do Windows Autopilot
+function Get-AutopilotProfiles {
+    $profiles = Get-MgBetaDeviceManagementWindowsAutopilotDeploymentProfile
+    return $profiles
+}
+
 # Função para obter informações do Windows Autopilot com GroupTag
 function Get-WindowsAutopilotInfoWithGroupTag($groupTag) {
     Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
@@ -27,33 +38,92 @@ Add-Type -AssemblyName System.Drawing
 $form = New-Object System.Windows.Forms.Form
 $form.Text = "Autopilot - Marcelo Goncalves v1.0 - Intune Lovers!"
 $form.Size = New-Object System.Drawing.Size(400, 400)
-$form.BackColor = [System.Drawing.Color]::Black
 
-# Função para centralizar os botões
-function CenterControl($control, $form) {
-    $control.Location = New-Object System.Drawing.Point(($form.ClientSize.Width - $control.Width) / 2, $control.Location.Y)
-}
+$buttonInstallGraph = New-Object System.Windows.Forms.Button
+$buttonInstallGraph.Text = "Install Microsoft Graph"
+$buttonInstallGraph.Location = New-Object System.Drawing.Point(100, 20)
+$buttonInstallGraph.Size = New-Object System.Drawing.Size(200, 30)
+$buttonInstallGraph.BackColor = [System.Drawing.Color]::Yellow
+$form.Controls.Add($buttonInstallGraph)
+
+$buttonLogin = New-Object System.Windows.Forms.Button
+$buttonLogin.Text = "Login"
+$buttonLogin.Location = New-Object System.Drawing.Point(150, 70)
+$buttonLogin.Size = New-Object System.Drawing.Size(100, 30)
+$form.Controls.Add($buttonLogin)
 
 $buttonAutopilotGroupTag = New-Object System.Windows.Forms.Button
 $buttonAutopilotGroupTag.Text = "Autopilot Online With Group Tag"
+$buttonAutopilotGroupTag.Location = New-Object System.Drawing.Point(100, 120)
 $buttonAutopilotGroupTag.Size = New-Object System.Drawing.Size(200, 30)
-$buttonAutopilotGroupTag.Location = New-Object System.Drawing.Point(0, 50)
+$buttonAutopilotGroupTag.Enabled = $false
 $form.Controls.Add($buttonAutopilotGroupTag)
-CenterControl $buttonAutopilotGroupTag $form
 
 $buttonAutopilotCSV = New-Object System.Windows.Forms.Button
 $buttonAutopilotCSV.Text = "Windows Autopilot CSV"
+$buttonAutopilotCSV.Location = New-Object System.Drawing.Point(100, 170)
 $buttonAutopilotCSV.Size = New-Object System.Drawing.Size(200, 30)
-$buttonAutopilotCSV.Location = New-Object System.Drawing.Point(0, 100)
 $buttonAutopilotCSV.Enabled = $false
 $form.Controls.Add($buttonAutopilotCSV)
-CenterControl $buttonAutopilotCSV $form
 
 $labelStatus = New-Object System.Windows.Forms.Label
-$labelStatus.Location = New-Object System.Drawing.Point(0, 20)
+$labelStatus.Location = New-Object System.Drawing.Point(150, 50)
 $labelStatus.Size = New-Object System.Drawing.Size(100, 20)
 $form.Controls.Add($labelStatus)
-CenterControl $labelStatus $form
+
+# Evento de clique do botão Install Microsoft Graph
+$buttonInstallGraph.Add_Click({
+    $installForm = New-Object System.Windows.Forms.Form
+    $installForm.Text = "Installing Microsoft Graph"
+    $installForm.Size = New-Object System.Drawing.Size(300, 100)
+
+    $labelInstall = New-Object System.Windows.Forms.Label
+    $labelInstall.Text = "Installing Microsoft Graph, please wait..."
+    $labelInstall.AutoSize = $true
+    $labelInstall.Location = New-Object System.Drawing.Point(10, 10)
+    $installForm.Controls.Add($labelInstall)
+
+    $installForm.Show()
+
+    Start-Process powershell -ArgumentList "Install-Module Microsoft.Graph -AllowClobber -Force" -Wait
+
+    $installForm.Close()
+    [System.Windows.Forms.MessageBox]::Show("Microsoft Graph installed successfully!", "Installation Complete", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+})
+
+# Evento de clique do botão de login
+$buttonLogin.Add_Click({
+    try {
+        Connect-MgGraphWithScopes
+        $buttonAutopilotGroupTag.Enabled = $true
+        $buttonAutopilotCSV.Enabled = $true
+        $successForm = New-Object System.Windows.Forms.Form
+        $successForm.Text = "Login Status"
+        $successForm.Size = New-Object System.Drawing.Size(300, 100)
+        $successForm.BackColor = [System.Drawing.Color]::Green
+
+        $labelSuccess = New-Object System.Windows.Forms.Label
+        $labelSuccess.Text = "SUCCESS"
+        $labelSuccess.AutoSize = $true
+        $labelSuccess.Location = New-Object System.Drawing.Point(10, 10)
+        $successForm.Controls.Add($labelSuccess)
+
+        $successForm.ShowDialog()
+    } catch {
+        $failForm = New-Object System.Windows.Forms.Form
+        $failForm.Text = "Login Status"
+        $failForm.Size = New-Object System.Drawing.Size(300, 100)
+        $failForm.BackColor = [System.Drawing.Color]::Red
+
+        $labelFail = New-Object System.Windows.Forms.Label
+        $labelFail.Text = "FAIL"
+        $labelFail.AutoSize = $true
+        $labelFail.Location = New-Object System.Drawing.Point(10, 10)
+        $failForm.Controls.Add($labelFail)
+
+        $failForm.ShowDialog()
+    }
+})
 
 # Evento de clique do botão Autopilot Online With Group Tag
 $buttonAutopilotGroupTag.Add_Click({
